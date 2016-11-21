f335a2267bd35ae7f529d356cc1c3ddb3518cbe0
Findley, Randy <rgfindley@gmail.com>
2016-11-21 14:05:49 -0500

    add timeout to lambda add test1.yml

--- lambda/handler.js
+++ lambda/handler.js
@@ -1,13 +1,20 @@
 'use strict';
+var _ = require('lodash');
 
 module.exports.test = (event, context, callback) => {
   const response = {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+      input: event
+    })
   };
 
-  callback(null, response);
+  if (_.isNil(event.query)) {
+    callback(null, response);
+  } else {
+    setTimeout(function() {
+      callback(null, response);
+    }, _.toNumber(event.query.timeout));
+  }
 };
