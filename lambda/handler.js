cb001d7a4f4d9754284a19f34b4485fbc2add7ec
Findley, Randy <rgfindley@gmail.com>
2016-12-12 13:07:32 -0500

    Add timeout to API Gateway requests.

--- lambda/handler.js
+++ lambda/handler.js
@@ -2,19 +2,27 @@
 var _ = require('lodash');
 
 module.exports.test = (event, context, callback) => {
+
+  var timeout = null;
+  if (!_.isNil(event.query) && !_.isNil(event.query.timeout))
+    timeout = event.query.timeout;
+  else if (!_.isNil(event.queryStringParameters) && !_.isNil(event.queryStringParameters.timeout))
+    timeout = event.queryStringParameters.timeout;
+
   const response = {
     statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event
+      input: event,
+      timeout: timeout
     })
   };
 
-  if (_.isNil(event) || _.isNil(event.query) || _.isNil(event.query.timeout)) {
+  if (_.isNil(timeout)) {
     callback(null, response);
   } else {
     setTimeout(function() {
       callback(null, response);
-    }, _.toNumber(event.query.timeout));
+    }, _.toNumber(timeout));
   }
 };
